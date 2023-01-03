eace1dd72780a6853f5f5223af94ee4bffb0e496
james-fleming394 <james.fleming394@gmail.com>
2023-01-03 17:49:25 -0500

    Database connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const db = require("./db");
 
 const app = express();
 const PORT = process.env.PORT || 7001;
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
