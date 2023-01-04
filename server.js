9a7a71aae0f3009a51cfb55d52bf8eb014fdb545
james-fleming394 <james.fleming394@gmail.com>
2023-01-04 14:19:34 -0500

    Routes basic connect

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 const db = require("./db");
+const routes = require('./routes/Routes')
 
 const app = express();
 const PORT = process.env.PORT || 7001;
@@ -12,6 +13,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.get('/', (req, res) => {
+    res.json({ message: "Server working"})
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
